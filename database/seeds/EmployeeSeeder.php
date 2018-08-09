d75478770cfba650bb3d857ee12735d71f237976
TOKU\Tetsuya <tetsuya1402@gmail.com>
2018-08-09 18:20:54 +0900

    seederコミット

--- database/seeds/EmployeeSeeder.php
+++ database/seeds/EmployeeSeeder.php
@@ -12,14 +12,20 @@ class EmployeeSeeder extends Seeder
     public function run()
     {
        DB::table('employees')->insert([
-        ['family_name'=>'月成','given_name'=>'健太','family_name_kana'=>'つきなり','given_name_kana'=>'けんた','beacon_id'=>'1']
+        ['family_name'=>'奥永','given_name'=>'哲哉','family_name_kana'=>'おくなが','given_name_kana'=>'てつや','beacon_id'=>'','position'=>'アジアクエスト']
        ]);
+        DB::table('employees')->insert([
+            ['family_name'=>'坂本','given_name'=>'勇人','family_name_kana'=>'さかもと','given_name_kana'=>'はやと','beacon_id'=>'','position'=>'東京ドーム']
+        ]);
+        DB::table('employees')->insert([
+            ['family_name'=>'千原','given_name'=>'ジュニア','family_name_kana'=>'ちはら','given_name_kana'=>'じゅにあ','beacon_id'=>'','position'=>'パソコンの前']
+        ]);
 
 
 
-
-       //       DB::table('beacons')->insert([
-//        ['major'=>'33',"minor"=>"4","position"=>"cafe"]
+//
+//              DB::table('beacons')->insert([
+//        ['major'=>'334',"minor"=>"44","position"=>"office"]
 //       ]);
 
     }
